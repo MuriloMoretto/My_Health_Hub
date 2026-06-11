6613f49ffbc700327262739cfb493f31d090a870
MuriloMoretto <murilobocaina2005@gmail.com>
2026-06-11 10:59:27 -0300

    fix: adiciona express.json middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors    = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 // ─── Rotas ────────────────────────────────────────────────────
 app.use('/api/auth',          require('./routes/auth'));
